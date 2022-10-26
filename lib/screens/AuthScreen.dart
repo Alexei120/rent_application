c3a9dff1711cfc5006b3d3e78f66a7e591236b76
Enot36525 <115657677+Alexei120@users.noreply.github.com>
2022-10-26 09:40:58 +0800

    1

---
+++ lib/screens/AuthScreen.dart
@@ -0,0 +1,38 @@
+import 'dart:html';
+
+import 'package:rent_application/theme/model_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ModelTheme>(
+        builder: (context, ModelTheme themeNotifier, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Авторизация'),
+          actions: [
+            IconButton(
+                icon: Icon(themeNotifier.isDark
+                    ? Icons.nightlight_round
+                    : Icons.wb_sunny),
+                onPressed: () {
+                  themeNotifier.isDark
+                      ? themeNotifier.isDark = false
+                      : themeNotifier.isDark = true;
+                })
+          ],
+        ),
+        body: Container(),
+      );
+    });
+  }
+}
